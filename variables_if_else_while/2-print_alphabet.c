34ace7b6f5909f1fd859cd04148c9f8c1ff5c383
Georgia Silva <georgelina2292@gmail.com>
2023-08-03 23:47:21 -0700

    Printing the alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a to z
+ * Return zero
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
