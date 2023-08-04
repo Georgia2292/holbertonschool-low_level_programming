023f32e18c6ddff31f59b370506b8af51c2594e2
Georgia Silva <georgelina2292@gmail.com>
2023-08-03 21:57:15 -0700

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,13 +1,14 @@
-nclude <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
- *  * * main - Entry point
- *   *  *
- *    *   *Return: always 0
- *     **/
+ *  *  * * main - Entry point
+ *   *   *  *
+ *    *    *   *Return: always 0
+ *     *     **/
 int main(void)
 {
+
 	int n;
 
 	srand(time(0));
