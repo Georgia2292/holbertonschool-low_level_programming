49094cb543db91037dc33a5565ed3770490811ff
Georgia Silva <georgelina2292@gmail.com>
2023-08-05 11:26:41 -0700

    Alpha in low and upper case

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prints a to z in caps and low case
+ *
+ *
+ * Return: zero
+ */
+
+int main(void)
+
+{
+	char  Lower = 'a';
+	char  Upper = 'A';
+
+	while (Lower <= 'z')
+	{
+		putchar(Lower);
+		Lower++;
+	}
+	while (Upper <= 'z')
+	{
+		putchar(Upper);
+		Upper++;
+	}
+	putchar('\n');
+	return (0);
+}
