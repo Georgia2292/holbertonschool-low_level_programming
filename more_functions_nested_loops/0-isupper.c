20d22045509a8b3fed69fe041fcb88f5254d0cf8
Georgia Silva <georgelina2292@gmail.com>
2023-08-17 23:27:19 -0700

    Using isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if a leller is upper
+ * @c: char to check
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+
+{
+
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
+
