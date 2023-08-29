c8cadd34e0e3083a258a052b5883028f4aad249a
Georgia Silva <georgelina2292@gmail.com>
2023-08-28 20:54:12 -0700

    Structures and typedef

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *  struct dog - struct that contains the dog´s info
+ *
+ *  @name: dog name
+ *  @age: dogs age
+ *  @owner: dogs owner
+ *
+ *  Return: Nothing
+ */
+struct dog {
+        char *name;
+        float age;
+        char *owner;
+
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
+
