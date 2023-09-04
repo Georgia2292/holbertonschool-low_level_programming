81c1c1fb7c28a0878678e79ebe0d8d2432be7e35
Georgia Silva <georgelina2292@gmail.com>
2023-09-03 17:06:37 -0700

    Defining a new type dog_t as a new name for the struct dog type.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  *  struct dog - struct that contains the dog´s info
  *
@@ -9,16 +14,32 @@
  *
  *  Return: Nothing
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
+} dog;
+
+/**
+ * struct dog_t - Structut for created a dog.
+ * @name: Name of dog.
+ * @age: Age of dog.
+ * @owner: Owner of dog.
+ */
+
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
 
