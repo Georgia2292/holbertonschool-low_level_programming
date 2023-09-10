df68e19ec043ebd9647fce99f6d75edc56581aae
Georgia Silva <georgelina2292@gmail.com>
2023-09-10 00:41:26 -0700

    A  program that performs simple operatio.

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,50 +1,47 @@
-#include "function_pointers.h"
-	#include <stdlib.h>
-	#include <stdio.h>
-	#include "3-calc.h"
-	/**
-	 * main - Prints the result of simple operations.
-	 * @argc: The number of arguments supplied to the program.
-	 * @argv: An array of pointers to the arguments.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(int __attribute__((__unused__)) argc, char *argv[])
+#include "3-calc.h"
+/**
+ * main - This program
+ * @argc: Numbers of args.
+ * @argv: Args (calc num1 operator num2)
+ *
+ * Return: Result of operation, otherwise any number and print Error.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char *operator;
+
+	operator = argv[2];
+
+	if (argc != 4)
 	{
-		int num1, num2;
-		char *op;
-
-
-		if (argc != 4)
-		{
-			printf("Error\n");
-			exit(98);
-		}
-
-
-		num1 = atoi(argv[1]);
-		op = argv[2];
-		num2 = atoi(argv[3]);
-
-
-		if (get_op_func(op) == NULL || op[1] != '\0')
-		{
-			printf("Error\n");
-			exit(99);
-		}
-
-
-		if ((*op == '/' && num2 == 0) ||
-		    (*op == '%' && num2 == 0))
-		{
-			printf("Error\n");
-			exit(100);
-		}
-
-
-		printf("%d\n", get_op_func(op)(num1, num2));
-
-
-		return (0);
+		puts("Error");
+		return (98);
 	}
+	switch (*operator)
+	{
+		case '+':
+			break;
+		case '-':
+			break;
+		case '*':
+			break;
+		case '/':
+			break;
+		case '%':
+			break;
+		default:
+			puts("Error");
+			return (99);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
+	if (num2 == 0 && (*operator == '%' || *operator == '/'))
+	{
+		puts("Error");
+		return (100);
+	}
+	result = (*get_op_func(operator))(num1, num2);
+	return (printf("%d\n", result));
+}
